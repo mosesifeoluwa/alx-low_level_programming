000adc8fce67e36d735d076cebe711fc8f49a835
mosesifeoluwa <mosesifeoluwa0906@gmail.com>
2023-03-16 16:20:44 -0700

    C-code_using_the_puts_function

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - C code that prints with a puts function
+ *
+ * Return: 0 (Always Sucess)
+*/
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
