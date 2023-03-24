0d012c6efbd95bf08393aea25cd11aa8bfe730b3
mosesifeoluwa <mosesifeoluwa0906@gmail.com>
2023-03-23 19:23:50 -0700

    More Functions, More Nested Loop

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -6,16 +6,16 @@
  */
 void print_line(int n)
 {
-	if (n <= 10)
+	if (n <= 0)
 	{
 		_putchar('\n');
 	} else
 	{
 		int i;
 
-		for (i = 1; i <= n; i++)
+		for (i = 0; i < n; i++)
 		{
-			_putchar('_');
+			_putchar(95);
 		}
 		_putchar('\n');
 	}
