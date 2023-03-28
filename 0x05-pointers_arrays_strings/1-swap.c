18e7cb0af981fb0a2a6d5392c97aae6eb4a6ca78
mosesifeoluwa <mosesifeoluwa0906@gmail.com>
2023-03-27 18:03:40 -0700

    Function to add two integers

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * swap_int - swaps the values of two int(s)
+ * @a: int to swap
+ * @b: int to swap
+ */
+
+void swap_int(int *a, int *b)
+{
+	int m;
+
+	m = *a;
+	*a = *b;
+	*b = m;
+}
